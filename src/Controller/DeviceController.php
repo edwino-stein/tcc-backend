cda618da09ccec2d6fb9f9a1379377e276bb8c43
Edwino Stein <edwino.stein@gmail.com>
2019-06-17 17:46:35 -0400

    Adicionado DeviceController

---
+++ src/Controller/DeviceController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Service\DeviceService;
+
+class DeviceController extends AbstractController {
+
+    /**
+     * @Route("/device", name="device")
+     */
+    public function index(){
+        return $this->render('frontend/index.html.twig');
+    }
+
+    /**
+     * @Route("/device/poweroff", name="device_poweroff")
+     */
+    public function poweroff(DeviceService $device){
+        return $device->poweroff();
+    }
+
+    /**
+     * @Route("/device/reboot", name="device_reboot")
+     */
+    public function reboot(DeviceService $device){
+        return $device->reboot();
+    }
+
+    /**
+     * @Route("/device/info", name="device_info")
+     */
+    public function info(DeviceService $device){
+
+        var_dump($device->getInfo());
+
+        return $this->render('device/index.html.twig', [
+            'controller_name' => 'DeviceController',
+        ]);
+    }
+}
